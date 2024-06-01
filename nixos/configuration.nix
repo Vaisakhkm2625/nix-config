1cdc3b1a98c86146a1c19993c0ee6caaac7b0611
Vaisakh K M <vaisakhkm2625@gmail.com>
2024-06-02 04:38:55 +0530

    before flake updating on Sunday 02 June 2024 04:38:55 AM IST

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -287,9 +287,7 @@ services.mysql = {
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
   environment.sessionVariables.WLR_NO_HARDWARE_CURSORS = "1";
 
-   stylix.image = ./wallpaper.png;
-
-
+  #stylix.image = ./wallpaper.png;
   #stylix.image = pkgs.fetchurl {
   #  url = "https://www.pixelstalk.net/wp-content/uploads/2016/05/Epic-Anime-Awesome-Wallpapers.jpg";
   #  sha256 = "enQo3wqhgf0FEPHj2coOCvo7DuZv+x5rL/WIo4qPI50=";
